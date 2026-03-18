cb6f44df16fa824a9778aa94f94fe58abe456751
ATWeatherly <waustin127@gmail.com>
2026-03-17 21:21:45 -0400

    increase TLB size

--- hw/rtl/core/VX_mmu_tlb.sv
+++ hw/rtl/core/VX_mmu_tlb.sv
@@ -95,8 +95,8 @@ module VX_mmu_tlb import VX_gpu_pkg::*; #(
     // TLB Logic
     // =========================================================================
 
-    localparam TLB_SIZE       = 32;
-    localparam TLB_INDEX_BITS = 5;
+    localparam TLB_SIZE       = 128;
+    localparam TLB_INDEX_BITS = 7;
     localparam PAGE_OFFSET_BITS = 12 - `CLOG2(DATA_SIZE);
     localparam VPN_WIDTH = 20;
     localparam PPN_WIDTH = VPN_WIDTH;
