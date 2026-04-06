896e505a93165ff41319e23528dbf09ad4504c87
ATWeatherly <waustin127@gmail.com>
2026-04-06 17:44:27 -0400

    mem offset fix

--- runtime/xrt/vortex.cpp
+++ runtime/xrt/vortex.cpp
@@ -661,11 +661,11 @@ public:
 
   int mpm_query(uint32_t addr, uint32_t core_id, uint64_t *value) {
     uint32_t offset = addr - VX_CSR_MPM_BASE;
-    if (offset > 31)
+    if (offset > 47)
       return -1;
     if (mpm_cache_.count(core_id) == 0) {
-      uint64_t mpm_mem_addr = IO_MPM_ADDR + core_id * 32 * sizeof(uint64_t);
-      CHECK_ERR(this->download(mpm_cache_[core_id].data(), mpm_mem_addr, 32 * sizeof(uint64_t)), {
+      uint64_t mpm_mem_addr = IO_MPM_ADDR + core_id * 48 * sizeof(uint64_t);
+      CHECK_ERR(this->download(mpm_cache_[core_id].data(), mpm_mem_addr, 48 * sizeof(uint64_t)), {
         return err;
       });
     }
@@ -682,7 +682,7 @@ private:
   uint64_t isa_caps_;
   uint64_t global_mem_size_;
   DeviceConfig dcrs_;
-  std::unordered_map<uint32_t, std::array<uint64_t, 32>> mpm_cache_;
+  std::unordered_map<uint32_t, std::array<uint64_t, 48>> mpm_cache_;
   uint32_t lg2_num_banks_;
   uint32_t lg2_bank_size_;
 
